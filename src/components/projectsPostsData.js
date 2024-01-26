47a819966733fbe629870e159295d8a97a72c8aa
Ben Math <benjamin.aaron.math@gmail.com>
2024-01-25 23:00:13 -0500

    updating deployment blog post

--- src/components/projectsPostsData.js
+++ src/components/projectsPostsData.js
@@ -1,6 +1,12 @@
 export const projectsPostsData = [
     // Add projects posts as needed
     {
+      title: "1, 2, 3 Deployed!",
+      subtitle: 'Deploying and setting up CI/CD for benmath.io',
+      content: './blogs/projects/123Deployment.html',
+      date: 'January 25, 2024',
+    },
+    {
       title: "Gotta Catch 'Em All - Bulding the Pokedex",
       subtitle: 'All the features, that is!',
       content: './blogs/projects/GottaCatchEmAll.html',
