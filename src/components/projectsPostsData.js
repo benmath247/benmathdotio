0d34177d96ae22836a7e9851f4d9cf6383b4320e
Ben Math <benjamin.aaron.math@gmail.com>
2024-01-27 10:46:18 -0500

    adding neighborhoods backend post

--- src/components/projectsPostsData.js
+++ src/components/projectsPostsData.js
@@ -1,22 +1,27 @@
 export const projectsPostsData = [
-    // Add projects posts as needed
-    {
-      title: "1, 2, 3 Deployed!",
-      subtitle: 'Deploying and setting up CI/CD for benmath.io',
-      content: './blogs/projects/123Deployment.html',
-      date: 'January 25, 2024',
-    },
-    {
-      title: "Gotta Catch 'Em All - Bulding the Pokedex",
-      subtitle: 'All the features, that is!',
-      content: './blogs/projects/GottaCatchEmAll.html',
-      date: 'January 20, 2024',
-    },
-    {
-      title: 'Scaling up the Brandsite Generator',
-      subtitle: 'Unearthing technical debt to make it a multi-page site solution',
-      content: './blogs/projects/BrandsiteGeneratorPages.html',
-      date: 'January 17, 2024',
-    },
-  ];
-  
+  // Add projects posts as needed
+  {
+    title: "The Neighborhoods Backend",
+    subtitle: 'Creating a backend to support front ends for neighborhood associations in Philly',
+    content: './blogs/projects/MakingNeighborhoodsBackend.html',
+    date: 'January 26, 2024',
+  },
+  {
+    title: "1, 2, 3 Deployed!",
+    subtitle: 'Deploying and setting up CI/CD for benmath.io',
+    content: './blogs/projects/123Deployment.html',
+    date: 'January 25, 2024',
+  },
+  {
+    title: "Gotta Catch 'Em All - Bulding the Pokedex",
+    subtitle: 'All the features, that is!',
+    content: './blogs/projects/GottaCatchEmAll.html',
+    date: 'January 20, 2024',
+  },
+  {
+    title: 'Scaling up the Brandsite Generator',
+    subtitle: 'Unearthing technical debt to make it a multi-page site solution',
+    content: './blogs/projects/BrandsiteGeneratorPages.html',
+    date: 'January 17, 2024',
+  },
+];
