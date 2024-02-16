6cd10aee56338a965a9491503797c234d642d2df
Ben Math <benjamin.aaron.math@gmail.com>
2024-02-15 22:29:07 -0500

    fixing hangman bug

--- src/projects/Hangman/Hangman.jsx
+++ src/projects/Hangman/Hangman.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const words = [
-  "bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon",    "charizard", "squirtle", "wartortle", "blastoise", "caterpie",    "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey",    "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow",    "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash",    "nidoran♀", "nidorina", "nidoqueen", "nidoran♂", "nidorino", "nidoking",    "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff",    "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect",    "venonat", "venomoth", "digglet", "dugtrio", "meowth", "persian", "psyduck",    "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl",    "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp",    "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude",    "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite",    "magneton", "farfetch'd", "doduo", "dodrio", "seel", "dewgong", "grimer",    "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee",    "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor",    "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing",    "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea",    "seadra", "goldeen", "seaking", "staryu", "starmie", "mr. mime", "scyther",    "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados",    "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon",    "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno",    "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"]
+  "bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran♀", "nidorina", "nidoqueen", "nidoran♂", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "digglet", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetch'd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mr. mime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"
+];
 
 
 function HangmanGame() {
@@ -12,6 +13,7 @@ function HangmanGame() {
   const [attempts, setAttempts] = useState(0);
   const [maxAttempts, setMaxAttempts] = useState(6);
   const [usedLetters, setUsedLetters] = useState([]);
+  const [pokemonData, setPokemonData] = useState(null);
 
   useEffect(() => {
     const randomWord = words[Math.floor(Math.random() * words.length)];
@@ -19,24 +21,20 @@ function HangmanGame() {
     setGuessedWord(new Array(randomWord.length).fill('_ '));
   }, []);
 
-  console.log("word")
-  console.log(word)
-  console.log("guessedWord.join('')")
-  console.log(word == guessedWord.join(''))
-
   const handleGuessChange = (event) => {
     setGuess(event.target.value);
   };
 
   const handleGuessSubmit = (event) => {
     event.preventDefault();
-    if (guess.length === 1 && !usedLetters.includes(guess)) {
+    const guessLowerCase = guess.toLowerCase();
+    if (guessLowerCase.length === 1 && !usedLetters.includes(guessLowerCase)) {
       const newGuessedWord = guessedWord.slice();
       let found = false;
 
       for (let i = 0; i < word.length; i++) {
-        if (word[i] === guess) {
-          newGuessedWord[i] = guess;
+        if (word[i] === guessLowerCase) {
+          newGuessedWord[i] = guessLowerCase;
           found = true;
         }
       }
@@ -47,7 +45,7 @@ function HangmanGame() {
 
       setGuessedWord(newGuessedWord);
       setGuess('');
-      setUsedLetters([...usedLetters, guess]);
+      setUsedLetters([...usedLetters, guessLowerCase]);
     }
   };
 
@@ -74,8 +72,6 @@ function HangmanGame() {
     );
   };
 
-  const [pokemonData, setPokemonData] = useState(null);
-
   const fetchPokemonData = async (pokemonName) => {
     try {
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
@@ -84,7 +80,7 @@ function HangmanGame() {
       console.error("Error fetching Pokemon data:", error);
     }
   };
-  
+
   const renderHint = () => {
     if (word) {
       fetchPokemonData(word.toLowerCase());
@@ -96,11 +92,11 @@ function HangmanGame() {
     return (
       <div className="container">
         <h2>Who's that Pokemon?!</h2>
-        <div style={{backgroundColor: "white"}}>
-          {!isGameWon && !isGameLost && <img src={pokemonData.sprites['front_shiny']} style={{ filter: 'brightness(0)' }}/>}
+        <div style={{ backgroundColor: "white" }}>
+          {!isGameWon && !isGameLost && <img src={pokemonData.sprites['front_shiny']} style={{ filter: 'brightness(0)' }} />}
           {(isGameWon) && <img src={pokemonData.sprites['front_shiny']} />}
           {(isGameLost) && <img src={pokemonData.sprites['front_shiny']} />}
-          </div>
+        </div>
         <p>Height: {pokemonData.height} decimetres</p>
         <p>Weight: {pokemonData.weight} hectograms</p>
         <p>Type: {pokemonData.types.map((type) => type.type.name).join(', ')}</p>
@@ -115,42 +111,37 @@ function HangmanGame() {
       <p>Try to guess the Pokemon by entering a letter at a time. You have {maxAttempts - attempts} attempts left.</p>
 
       {isGameLost && <>
-      <p className="text-danger">You lost! The word was "{word}".</p>
+        <p className="text-danger">You lost! The word was "{word}".</p>
       </>}
       {isGameWon && <p className="text-success">Congratulations! You guessed the word: "{word}".</p>}
 
-        <>
-          {renderWord()}
-          {renderUsedLetters()}
-          <form onSubmit={handleGuessSubmit}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter a letter"
-                value={guess}
-                onChange={handleGuessChange}
-                maxLength="1"
-                />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Guess
+      <>
+        {renderWord()}
+        {renderUsedLetters()}
+        <form onSubmit={handleGuessSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter a letter"
+              value={guess}
+              onChange={handleGuessChange}
+              maxLength="1"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Guess
+          </button>
+          <div>
+            <button className="btn btn-primary" onClick={() => window.location.reload(false)}>
+              New Game
             </button>
-                
-                  <div>
-                  <button className="btn btn-primary" onClick={() => window.location.reload(false)}>
-                    New Game
-                  </button></div>
-                  
-
-      {renderHint()}
-          </form>
-        </>
-      
+          </div>
+          {renderHint()}
+        </form>
+      </>
     </div>
   );
 }
 
 export default HangmanGame;
-
-
