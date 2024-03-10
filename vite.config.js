7f521f063d7aaa6b3193cc959f9b8142f0402b00
Ben Math <benjamin.aaron.math@gmail.com>
2024-03-10 18:33:53 -0400

    updating config for unity

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression({
+      // Options for the compression plugin
+      verbose: true, // Enable verbose logging
+      disable: false, // Enable/disable compression (true/false)
+      threshold: 10240, // File size threshold for compression in bytes (default is 10kb)
+      algorithm: 'gzip', // Compression algorithm ('gzip', 'brotli')
+      ext: '.gz' // Extension for compressed files
+    })
+  ]
+});
