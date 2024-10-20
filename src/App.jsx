721c379e29bf5b29c68a7614bd48b87dac8f18ce
Ben Math <benjamin.aaron.math@gmail.com>
2024-10-20 13:57:12 -0400

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import Translation from './projects/Translation/Translation';
 import Dictation from './projects/Dictation/dictation';
 import Pokedex from './projects/Pokedex/Pokedex';
 import ContactForm from './pages/Contact';
-import FaceAnalysis from './projects/FaceAnalysis/FaceAnalysis';
+// import FaceAnalysis from './projects/FaceAnalysis/FaceAnalysis';
 import PokemonDetailsPage from './projects/Pokedex/PokemonDetails';
 import PokedexType from './projects/Pokedex/PokedexType';
 import DaysSinceQuit from './projects/QuitSmoking/DaysSinceQuit';
@@ -110,7 +110,7 @@ function App() {
         <Route path="/numbers" element={<NumberGuessingGame />} />
         <Route path="/pokedex" element={<Pokedex />} />
         <Route path="/foods" element={<Foods />} />
-        <Route path="/face-recognition" element={<FaceAnalysis />} />
+        {/* <Route path="/face-recognition" element={<FaceAnalysis />} /> */}
         <Route path="/rothira" element={<Retirement />} />
         <Route path="/pokedex/type/:id" element={<PokedexType />} />
         <Route path="/pokedex/pokemon/:id" element={<PokemonDetailsPage />} />
@@ -118,6 +118,7 @@ function App() {
         <Route path="/days-since-quit" element={<DaysSinceQuit />} />
         <Route path="/workoutPlanner" element={<ExampleCall />} />
         <Route path="/apininjas" element={<APINinjasNav />} />
+        <Route path="/kristynas-site" element={<Home />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
