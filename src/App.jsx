5e02a901c1d973b1e24db6bec8df8cf22dc3dfc1
Ben Math <benjamin.aaron.math@gmail.com>
2025-02-10 21:23:39 -0500

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import CustomNavBar from './components/navBar/CustomNavBar';
 import Home from './pages/Home';
 import About from './pages/About';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import Blog from './pages/Blog';
 import Portfolio from './pages/Portfolio';
 import APINinjasNav from './projects/APINinjas/ApiNinjas';
-import './App.css';
-import { FaGithub, FaLinkedin, FaFilePdf } from 'react-icons/fa';
+import { allPostsData } from './components/allPostsData';
+import BlogDetails from './pages/BlogDetails';
+import ContactForm from './pages/Contact';
 import FormatJson from './projects/JsonPretty/FormatJson';
 import TikTacToe from './projects/TikTacToe/TikTacToe';
 import HangmanGame from './projects/Hangman/Hangman';
@@ -19,71 +18,15 @@ import MemoryGame from './projects/Memory/Memory';
 import Translation from './projects/Translation/Translation';
 import Dictation from './projects/Dictation/dictation';
 import Pokedex from './projects/Pokedex/Pokedex';
-import ContactForm from './pages/Contact';
 import PokemonDetailsPage from './projects/Pokedex/PokemonDetails';
 import PokedexType from './projects/Pokedex/PokedexType';
-import { allPostsData } from './components/allPostsData';
-import BlogDetails from './pages/BlogDetails';
 import Foods from './projects/FoodAPI/Foods';
 import Retirement from './projects/Retirement/Retirement';
-import { GITHUB_URL, LINKEDIN_URL, GOOGLE_DOCS_URL } from './constants';
 
 function App() {
-  const handleNavLinkClick = () => {
-    const offcanvasElement = document.querySelector('.offcanvas');
-    const bsOffcanvas = new window.bootstrap.Offcanvas(offcanvasElement);
-    bsOffcanvas.hide();
-  };
-
   return (
     <Router>
-      <Navbar expand="lg" className="mb-0 pb-0 border border-1" bg="dark">
-        <Navbar.Brand className='p-2' href="/">BENMATH.COM</Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="end"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">BENMATH.COM</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content flex-grow-1 pe-3">
-              <Nav.Link as={Link} to="/blog" onClick={handleNavLinkClick}>Blog</Nav.Link>
-              <Nav.Link as={Link} to="/portfolio" onClick={handleNavLinkClick}>Portfolio</Nav.Link>
-              <Nav.Link as={Link} to="/contact" onClick={handleNavLinkClick}>Contact</Nav.Link>
-            </Nav>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link
-                href={GITHUB_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={handleNavLinkClick}
-              >
-                <FaGithub className="rotate-icon" size={20} />
-              </Nav.Link>
-              <Nav.Link
-                href={LINKEDIN_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={handleNavLinkClick}
-              >
-                <FaLinkedin className="rotate-icon" size={20} />
-              </Nav.Link>
-              <Nav.Link
-                href={GOOGLE_DOCS_URL}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={handleNavLinkClick}
-              >
-                <FaFilePdf className="rotate-icon" size={20} />
-              </Nav.Link>
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Navbar>
-
+      <CustomNavBar />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog allPostsData={allPostsData} />} />
---
+++ src/components/navBar/CustomNavBar.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { FaGithub, FaLinkedin, FaFilePdf } from 'react-icons/fa';
+import { GITHUB_URL, LINKEDIN_URL, GOOGLE_DOCS_URL } from '../../constants';
+
+const Brand = () => (
+    <Navbar.Brand className="p-2" href="/">BENMATH.COM</Navbar.Brand>
+);
+
+const NavLinks = ({ handleNavLinkClick }) => (
+    <Nav className="justify-content flex-grow-1 pe-3">
+        <Nav.Link as={Link} to="/blog" onClick={handleNavLinkClick}>Blog</Nav.Link>
+        <Nav.Link as={Link} to="/portfolio" onClick={handleNavLinkClick}>Portfolio</Nav.Link>
+        <Nav.Link as={Link} to="/contact" onClick={handleNavLinkClick}>Contact</Nav.Link>
+    </Nav>
+);
+
+const SocialLinks = ({ handleNavLinkClick }) => (
+    <Nav className="justify-content-end flex-grow-1 pe-3">
+        <Nav.Link href={GITHUB_URL} target="_blank" rel="noopener noreferrer" onClick={handleNavLinkClick}>
+            <FaGithub className="rotate-icon" size={20} />
+        </Nav.Link>
+        <Nav.Link href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer" onClick={handleNavLinkClick}>
+            <FaLinkedin className="rotate-icon" size={20} />
+        </Nav.Link>
+        <Nav.Link href={GOOGLE_DOCS_URL} target="_blank" rel="noopener noreferrer" onClick={handleNavLinkClick}>
+            <FaFilePdf className="rotate-icon" size={20} />
+        </Nav.Link>
+    </Nav>
+);
+
+const CustomOffcanvas = ({ handleNavLinkClick }) => (
+    <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
+        <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">BENMATH.COM</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+            <NavLinks handleNavLinkClick={handleNavLinkClick} />
+            <SocialLinks handleNavLinkClick={handleNavLinkClick} />
+        </Offcanvas.Body>
+    </Navbar.Offcanvas>
+);
+
+const CustomNavBar = () => {
+    const handleNavLinkClick = () => {
+        const offcanvasElement = document.querySelector('.offcanvas');
+        if (offcanvasElement) {
+            const bsOffcanvas = new window.bootstrap.Offcanvas(offcanvasElement);
+            bsOffcanvas.hide();
+        }
+    };
+
+    return (
+        <Navbar expand="lg" className="mb-0 pb-0 border border-1" bg="dark">
+            <Brand />
+            <Navbar.Toggle aria-controls="offcanvasNavbar" />
+            <CustomOffcanvas handleNavLinkClick={handleNavLinkClick} />
+        </Navbar>
+    );
+};
+
+export default CustomNavBar;
