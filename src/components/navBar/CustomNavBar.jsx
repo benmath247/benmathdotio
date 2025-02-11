68be5fdf31017b7c8e6e57ffc1af9d3592f4cf71
Ben Math <benjamin.aaron.math@gmail.com>
2025-02-10 21:34:29 -0500

    code cleanup

--- src/components/navBar/CustomNavBar.jsx
+++ src/components/navBar/CustomNavBar.jsx
@@ -13,7 +13,7 @@ const Brand = () => (
 const NavLinks = ({ handleNavLinkClick }) => (
     <Nav className="justify-content flex-grow-1 pe-3">
         <Nav.Link as={Link} to="/blog" onClick={handleNavLinkClick}>Blog</Nav.Link>
-        <Nav.Link as={Link} to="/portfolio" onClick={handleNavLinkClick}>Portfolio</Nav.Link>
+        {/* <Nav.Link as={Link} to="/portfolio" onClick={handleNavLinkClick}>Portfolio</Nav.Link> */}
         <Nav.Link as={Link} to="/contact" onClick={handleNavLinkClick}>Contact</Nav.Link>
     </Nav>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import TextColorChanging from '../components/TextColorChanging';
+// import TextColorChanging from '../components/TextColorChanging';
 import FeaturedProjects from '../components/FeaturedProjects';
 import ParallaxBackground from '../components/ParallaxBackground';
 import Footer from '../components/Footer';
 
-
 function Home() {
-  const images = [];
-  for (let i = 1; i <= 151; i++) {
-    images.push(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`);
-  }
-
   return (
-
     <div className='background'>
       <section className="hero background py-5">
         <div style={{ paddingTop: "50px" }} className="container text-center">
@@ -27,29 +20,19 @@ function Home() {
           <h3 className="lead">
             Venture within for thoughts on my personal and professional life or to see projects I have worked on.
           </h3>
-
-          {/* <p>
-            I have a passion for coding with JavaScript, Python, and Java.
-            I enjoy working on projects that make a difference for my community, customers, and the world.
-          </p> */}
           <div>
-            <Link
+            {/* <Link
               to="/portfolio"
-              // className="btn btn-light btn-lg grow-on-hover fun-button"
               onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
             >
               <button className='m-2'>
-
                 View My Portfolio
               </button>
-            </Link>
+            </Link> */}
             <Link
-              to="/blog"
-              // className="btn btn-light btn-lg grow-on-hover fun-button-alt"
               onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
             >
               <button className='secondary m-2'>
-
                 View My Blog
               </button>
             </Link>
